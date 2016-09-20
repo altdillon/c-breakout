4dc1ed548a67b0701f04332fb94eb5506766b183
Dillon <dmanhall@gmail.com>
2016-09-19 23:29:30 -0600

    added build script

---
+++ breakout_compile.sh
@@ -0,0 +1,3 @@
+g++ -I/usr/include/x86_64-linux-gnu -g   main.cpp ball.cpp block.cpp bat.cpp spriteException.cpp sprite.cpp sprite.h InutExceptionClass.cpp gameboard.h game_elements.h gameboard.cpp exceptions.h -lallegro_image -lallegro_primitives -lallegro
+#./a.out
+rm *.gch
